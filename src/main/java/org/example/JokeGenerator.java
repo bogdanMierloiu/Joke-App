f51dfcda03d03718b5f2696f5f13cbd9632f41fe
Bogdan Mierloiu <bogdan.mierloiu@accesa.eu>
2023-07-18 13:30:26 +0300

    get joke book

---
+++ src/main/java/org/example/JokeGenerator.java
@@ -0,0 +1,11 @@
+package org.example;
+
+import java.io.IOException;
+
+public class JokeGenerator {
+
+    private static final String JOKE_API_URL = "https://official-joke-api.appspot.com/random_joke";
+
+
+
+}
